5d58302f53eb99fcbdaab39aa13498b80cc8d1b4
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-23 00:42:43 -0500

    setting the file base

---
+++ 1-Decisions/challenge1.js
@@ -0,0 +1,9 @@
+// variables
+const title = document.querySelector("h1");
+const description = document.querySelector("h3");
+title.innerText = "Challenge 1";
+//Challenge
+description.innerHTML = "Read a Number and determine if that number ends in 4";
+
+// Read the number
+// const number = prompt();
