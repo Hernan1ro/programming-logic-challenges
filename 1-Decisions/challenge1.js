6124f3ddc09974ad80ff3bde19d89dc09c34282d
Hernán D. Mercado <hernandmf@gmail.com>
2021-12-14 07:43:39 -0500

    First challenge completed

--- 1-Decisions/challenge1.js
+++ 1-Decisions/challenge1.js
@@ -6,4 +6,13 @@ title.innerText = "Challenge 1";
 description.innerHTML = "Read a Number and determine if that number ends in 4";
 
 // Read the number
-// const number = prompt();
+let number = 123123;
+
+stringNumber = number.toString();
+NumberArray = stringNumber.slice(stringNumber.lenght - 1, 1);
+
+if (NumberArray === "4") {
+  console.log("El número termina en 4");
+} else {
+  console.log("El número no terminna en cuatro");
+}
